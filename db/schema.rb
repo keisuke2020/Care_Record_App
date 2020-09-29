9fead56bd4c85211193c14be52c9a49f59d7ea3f
keisuke2020 <kei_a_study@yahoo.co.jp>
2020-09-29 11:43:11 +0900

    新規登録、ログイン機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,6 @@
 ActiveRecord::Schema.define(version: 2020_09_28_140929) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "phone_number", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
